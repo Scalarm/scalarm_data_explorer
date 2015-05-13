f343f1a5a4f3dd478766ff0d63a60709d361594d
scalarm <test@test.test>
2015-05-13 10:25:22 +0200

    Using base_url; added CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  
+  before_filter :add_cors_header
+
+  def add_cors_header
+    response['Access-Control-Allow-Origin'] = '*'
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ resources :panels do
  end
 
  resources :scripts 
+resources :moes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
