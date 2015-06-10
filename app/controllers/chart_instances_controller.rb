4463cccc5450494d4d7133760e5a436193cc13f5
Andriu01 <romanski.andrzej@gmail.com>
2015-06-10 13:05:51 -0700

    adding moes and input parameters for Chart

--- app/controllers/chart_instances_controller.rb
+++ app/controllers/chart_instances_controller.rb
@@ -11,16 +11,19 @@ class ChartInstancesController < ApplicationController
     param1 = params[:param1].to_s
     param2 = params[:param2].to_s
 
-    parameters = { "id" => chart_id }
-    parameters["param1"] = param1
-    parameters["param2"] = param2
-    parameters["chart_id"] = chart_counter
-    parameters["output"] = output
-    parameters["type"] = type
-
+    filter = {is_done: true, is_error: {'$exists'=> false}}
+    fields = {fields: {result: 1}}
+    @parameters = { "id" => chart_id }
+    @parameters["param1"] = param1
+    @parameters["param2"] = param2
+    @parameters["chart_id"] = chart_counter
+    @parameters["output"] = output
+    @parameters["type"] = type
+    @parameters["moes"] = @experiment.simulation_runs.where(filter, fields).first.result
+    @parameters["input_parameters"]= @experiment.get_parameter_ids
     #require("visualisation_methods/#{chart_id}/#{chart_id}")
 
-
+# dodac moes oraz input parameters
   	#if @experiment.visible_to(@current_user.id)
       path = Rails.root.join('app','visualisation_methods',"#{chart_id}","plugin")
       require(path)
