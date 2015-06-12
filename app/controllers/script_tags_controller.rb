3058ae5b66dd86c3b42d438e19fc3fc100056a64
khalska <katarzynahalska@wp.pl>
2015-06-12 11:36:03 +0200

    fixing header showing

--- app/controllers/script_tags_controller.rb
+++ app/controllers/script_tags_controller.rb
@@ -2,8 +2,7 @@ class ScriptTagsController < ApplicationController
   PREFIX = "/"
   def show
     #rendering hmtl script for experiment
-    @prefix = params[:base_url] || PREFIX
-    render html: "<script type=\"text/javascript\" src=\"#{@prefix}scripts/#{params[:id]}/\">"
+    render html: "<script type=\"text/javascript\" src=\"#{@prefix}/scripts/#{params[:id]}\">"
 
 	end
 	
