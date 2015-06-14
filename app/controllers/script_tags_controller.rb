238f96218522dbc315c7cbd0500b0cc04b055c06
Andriu01 <romanski.andrzej@gmail.com>
2015-06-14 16:27:07 -0700

    fixing lindev modal and chart haml files

--- app/controllers/script_tags_controller.rb
+++ app/controllers/script_tags_controller.rb
@@ -2,7 +2,7 @@ class ScriptTagsController < ApplicationController
   PREFIX = "/"
   def show
     #rendering hmtl script for experiment
-    render html: "<script type=\"text/javascript\" src=\"#{@prefix}/scripts/#{params[:id]}\">"
+    render html: "<script type=\"text/javascript\" src=\"#{@prefix}/scripts/#{params[:id]}\"/>"
 
 	end
 	
--- app/visualisation_methods/lindev/plugin.rb
+++ app/visualisation_methods/lindev/plugin.rb
@@ -2,15 +2,9 @@ class Lindev
   attr_accessor :experiment
   attr_accessor :parameters
 
-  INFO = {
-      'name' => 'Line chart',
-      'id' => 'lindevModal',
-      'group' => 'basic',
-      'image' => '/chart/images/material_design/lindev_icon.png',
-      'description' => 'Line chart with standard deviation'
-  }
 
-  def prepare_lindev_chart_content(parameters, data)
+
+  def prepare_lindev_chart_content(data)
              output = "<script>(function() { \nvar i=" + parameters["chart_id"] + ";"
              #output += "\nvar data = " + JSON.stringify(data) + ";"
              output += "\nvar data = " + ActiveSupport::JSON.decode(data) + ";" if data != nil
@@ -18,7 +12,7 @@ class Lindev
              output += "\nlindev_main(i, \"" + parameters["param1"] + "\", \"" + parameters["param2"] + "\", data);"
              output += "\n})();</script>"
 
-             return output
+             output
 
 
   end
@@ -53,50 +47,54 @@ class Lindev
       data.arguments = new_args
       remove_instance_variable(data.values)
       data.result.each do |key|
-        data.result[key] = Float(data.result[key]) unless data.result[key].is_a? Float
+        data.result[key] = data.result[key].to_f unless data.result[key].is_a? Float
       end
       data
     end
 
-    mins = []
-    maxes = []
-    args.each do |i|
-      mins[args[i]] = min { |array, args|}
-      maxes[args[i]] = max { |array, args|}
-    end
+    # mins = []
+    # maxes = []
+    # args.each do |i|
+    #   mins[args[i]] = [array, args[i]].min
+    #   maxes[args[i]] = [array, args[i]].max
+    # end
+
+
 
     get_param1 = getter(param1, args)
     get_param2 = getter(param2, args)
     grouped_by_param1 = {}
-      array.map do |obj|
-        if grouped_by_param1.include? get_param1(obj)
-          grouped_by_param1[get_param1(obj)].push(get_param2(obj))
-        else grouped_by_param1[get_param1(obj)] = get_param2(obj)
-        end
-        array
+
+    array.map do |obj|
+      if grouped_by_param1.include?(get_param1(obj))
+        grouped_by_param1[get_param1(obj)].push(get_param2(obj))
+      else grouped_by_param1[get_param1(obj)] = get_param2(obj)
       end
+        array
+    end
 
 
-    # values = []
-    # grouped_by_param1.each do |index|
-    #   sum = grouped_by_param1[index].reduce(1, :+)
-    #   mean = sum/grouped_by_param1[index].length
-    #   values.push(Float(index, mean))
-    # end
-    # values = values.sort_by{|e| e}
-    # with_stddev = []
-    # grouped_by_param1.each do |index|
-    #   sum = grouped_by_param1[index].reduce(1, :+)
-    #   mean = sum/grouped_by_param1[index].length
-    #   partial_sd = 0
-    #   grouped_by_param1[index].each do |i|
-    #     partial_sd += **(grouped_by_param1[index][i]-mean)
-    #   end
-    #   sd = Math.sqrt(partial_sd/grouped_by_param1[index].length)
-    #   with_stddev.push(Float([index, mean-sd, mean+sd]))
-    # end
-    # with_stddev = with_stddev.sort_by{|e| e}
-    #
+    values = []
+    grouped_by_param1.each do |index|
+      sum = grouped_by_param1[index].reduce(:+)
+      mean = sum/grouped_by_param1[index].length
+      values.push([index.to_f, mean])
+     end
+
+    values = values.sort_by{|e| e}
+    with_stddev = []
+    grouped_by_param1.each do |index|
+      sum = grouped_by_param1[index].reduce(:+)
+      mean = sum/grouped_by_param1[index].length
+      partial_sd = 0
+      grouped_by_param1[index].each do |i|
+        partial_sd += (grouped_by_param1[index][i]-mean)**2
+       end
+       sd = Math.sqrt(partial_sd/grouped_by_param1[index].length)
+       with_stddev.push([index.to_f, mean-sd, mean+sd])
+    end
+    with_stddev = with_stddev.sort_by{|e| e}
+    [values, with_stddev]
     # success([values, with_stddev])
     rescue
       nil
@@ -118,7 +116,7 @@ class Lindev
 
         object = content[JSON.stringify(data)]
       elsif parameters["chart_id"]
-        object[:content] = prepare_lindev_chart_content(parameters, data)
+        object = prepare_lindev_chart_content(data)
         #object[:input_parameters] = retreived_parameters["parameters"]
         #object[:moes] = retreived_parameters["result"]
 
@@ -126,7 +124,8 @@ class Lindev
         error("Request parameters missing: 'chart_id'");
 
       end
-      getLineDev(experiment, parameters["id"], parameters["param1"], parameters["param2"])
+      object
+      #getLineDev(experiment, parameters["id"], parameters["param1"], parameters["param2"])
 
     end
   end
