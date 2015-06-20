8b63a8f40c2c8721920666dcfb97abe63454476b
Andriu01 <romanski.andrzej@gmail.com>
2015-06-20 08:17:24 -0700

    Fixing old charts and refactoring plugin for lindev

--- app/visualisation_methods/lindev/plugin.rb
+++ app/visualisation_methods/lindev/plugin.rb
@@ -3,6 +3,23 @@ class Lindev
   attr_accessor :parameters
 
 
+  def handler
+    if parameters["id"] && parameters["param1"] && parameters["param2"]
+
+      object = {}
+      data = get_line_dev_data(experiment, parameters["id"], parameters["param1"], parameters["param2"])
+      if parameters["type"] == "data"
+        object = content[JSON.stringify(data)]
+      elsif parameters["chart_id"]
+        object = prepare_lindev_chart_content(data)
+      else
+        error("Request parameters missing: 'chart_id'");
+      end
+      object
+
+    end
+  end
+
 
   def prepare_lindev_chart_content(data)
     output = "<script>(function() { \nvar i=" + parameters["chart_id"] + ";"
@@ -16,35 +33,16 @@ class Lindev
   end
 
 
-
-
-  def getter(param, args)
-    (args.indexOf(param) < 0) ? result[param] : arguments[param]
-  end
-
-
-  ##
   # TODO: documentation - what this method does? change name
-  def get_line_dev (experiment, id, param1, param2)
-    #dao.getData(id, function(array, args, mins, maxes){
+  def get_line_dev_data (experiment, id, param1, param2)
 
-    ## TODO
     simulation_runs = experiment.simulation_runs.to_a
 
-    # if array.length == 0
-    #   error("No such experiment or no runs done")
-    # end
-    # Rails.logger.debug("################")
-    # Rails.logger.debug(array)
-    # Rails.logger.debug("################")
-    # Rails.logger.debug(array.first)
-    # Rails.logger.debug("################")
-    # Rails.logger.debug(array.first.arguments)
+    if simulation_runs.length == 0
+      error("No such experiment or no runs done")
+    end
+
     argument_ids = simulation_runs.first.arguments.split(',')
-    #values = array.values.split(',')
-    # Rails.logger.debug("################")
-    # Rails.logger.debug(args)
-    # Rails.logger.debug(values)
 
     simulation_runs = simulation_runs.map do |data|
       obj ={}
@@ -54,9 +52,7 @@ class Lindev
       argument_ids.each_with_index do |arg_name, index|
         new_args[arg_name] = values[index].to_f
       end
-      # Rails.logger.debug("$$$$$$$$$$$$$$$$$")
-      # Rails.logger.debug(new_args)
-      #
+
       obj[:arguments] = new_args
       obj[:result] = {}
       #remove_instance_variable(data.values)
@@ -69,23 +65,59 @@ class Lindev
       obj
     end
 
-    # Rails.logger.debug("################")
-    # Rails.logger.debug(array)
-    # mins = []
-    # maxes = []
-    # args.each do |i|
-    #   mins[args[i]] = [array, args[i]].min
-    #   maxes[args[i]] = [array, args[i]].max
-    # end
-
-
-
-    # get_param1 = getter(param1, args)
-    #get_param2 = getter(param2, args)
-
-
     grouped_by_param1 = {}
 
+    #filling grouped_by_param1 with correct data
+    grouping_by_parameter(argument_ids, grouped_by_param1, param1, param2, simulation_runs)
+
+    values = []
+    #creating result table -> mean values
+    values = calculate_mean_values(grouped_by_param1, values)
+
+    with_stddev = []
+    #creating result table -> standard deviation values
+    with_stddev = calculate_standard_deviation(grouped_by_param1, with_stddev)
+
+    [values, with_stddev]
+
+  end
+
+
+  def calculate_standard_deviation(grouped_by_param1, with_stddev)
+    grouped_by_param1.each do |key, value|
+      sum = value.kind_of?(Array) ? value.reduce(:+) : value
+      mean = value.kind_of?(Array) ? sum/value.length : sum
+      partial_sd = 0
+      if value.kind_of?(Array)
+        value.each do |i|
+          partial_sd += (i-mean)**2
+        end
+      else
+        partial_sd = (value-mean)**2
+      end
+      sd = value.kind_of?(Array) ? Math.sqrt(partial_sd/value.length) : Math.sqrt(partial_sd)
+      with_stddev.push([key.to_f, mean-sd, mean+sd])
+    end
+    with_stddev = with_stddev.sort_by { |e| e }
+
+    with_stddev
+  end
+
+
+  def calculate_mean_values(grouped_by_param1, values)
+    grouped_by_param1.each do |key, value|
+      sum = value.kind_of?(Array) ? value.reduce(:+) : value
+      mean = value.kind_of?(Array) ? sum/value.length : sum
+      values.push([key.to_f, mean])
+
+    end
+    values = values.sort_by{|e| e}
+
+    values
+  end
+
+
+  def grouping_by_parameter(argument_ids, grouped_by_param1, param1, param2, simulation_runs)
     simulation_runs = simulation_runs.map do |obj|
       ## search for parameter value value in result or arguments
       param1_val = argument_ids.index(param1) ? obj[:arguments][param1] : obj[:result][param1]
@@ -98,68 +130,9 @@ class Lindev
       end
       obj
     end
-
-    Rails.logger.debug(grouped_by_param1)
-
-
-    values = []
-    grouped_by_param1.each do |key,value|
-      sum = value.kind_of?(Array) ? value.reduce(:+) : value
-      mean = value.kind_of?(Array) ? sum/value.length : sum
-      values.push([key.to_f, mean])
-    end
-    Rails.logger.debug(values)
-
-    values = values.sort_by{|e| e}
-    with_stddev = []
-    grouped_by_param1.each do |key,value|
-      sum = value.kind_of?(Array) ? value.reduce(:+) : value
-      mean = value.kind_of?(Array)? sum/value.length : sum
-      partial_sd = 0
-      if value.kind_of?(Array)
-        value.each do |i|
-          partial_sd += (i-mean)**2
-        end
-      else
-        partial_sd = (value-mean)**2
-      end
-      sd = value.kind_of?(Array)? Math.sqrt(partial_sd/value.length) : Math.sqrt(partial_sd)
-      with_stddev.push([key.to_f, mean-sd, mean+sd])
-    end
-    with_stddev = with_stddev.sort_by{|e| e}
-
-    [values, with_stddev]
-    # success([values, with_stddev])
   end
 
 
 
-
-
-
-
-  def handler
-    # dane parameters success error
-    if parameters["id"] && parameters["param1"] && parameters["param2"]
-
-      object = {}
-      data = get_line_dev(experiment, parameters["id"], parameters["param1"], parameters["param2"])
-      if parameters["type"] == "data"
-
-        object = content[JSON.stringify(data)]
-      elsif parameters["chart_id"]
-        object = prepare_lindev_chart_content(data)
-        #object[:input_parameters] = retreived_parameters["parameters"]
-        #object[:moes] = retreived_parameters["result"]
-
-      else
-        error("Request parameters missing: 'chart_id'");
-
-      end
-      object
-      #getLineDev(experiment, parameters["id"], parameters["param1"], parameters["param2"])
-
-    end
-  end
 
 end
