4c88cdcdfb0b01d38577158182ca69f034f0a8cf
Jakub Liput <jakub.liput@gmail.com>
2015-06-22 17:26:42 +0200

    Disabling protect from forgery and enabling JS invocation to make service available in Chrome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  #protect_from_forgery with: :null_session
 
   before_filter :authenticate, except: :status
   rescue_from Scalarm::ServiceCore::AuthenticationError, with: :authentication_failed
