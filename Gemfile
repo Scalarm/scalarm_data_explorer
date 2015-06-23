05cdd6b7aae6e9bd4481bde6709409a65fb6ff7a
Andriu01 <romanski.andrzej@gmail.com>
2015-06-23 10:13:26 -0700

    Get data from db and invoke R hclust method

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.1.1'
 # use puma as the app server
 gem 'puma'
 
+# wrapper for R interpreter
+gem 'rinruby'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- app/visualisation_methods/dendrogram/plugin.rb
+++ app/visualisation_methods/dendrogram/plugin.rb
@@ -1,26 +1,8 @@
+require 'rinruby'
+require 'csv'
 class Dendrogram
   attr_accessor :experiment
   attr_accessor :parameters
-
-
-
-  def prepare_dendrogram_chart_content(data)
-    output = "<script>(function() { \nvar i=" + parameters["chart_id"] + ";"
-    output += "\nvar data = " + data.to_json + ";" if data != nil
-    output += "\nwindow.dendrogram_main(i, \"" + parameters["param1"] + "\", data);"
-    output += "\n})();</script>"
-
-    output
-  end
-
-  ##
-  # TODO: documentation - what this method does? change name
-  def get_data_for_dendrogram(experiment, id, param1)
-    simulation_runs = experiment.simulation_runs.to_a
-
-
-  end
-
 
   def handler
     if parameters["id"] && parameters["param1"]
@@ -40,4 +22,112 @@ class Dendrogram
     end
   end
 
+  def prepare_dendrogram_chart_content(data)
+    output = "<script>(function() { \nvar i=" + parameters["chart_id"] + ";"
+    output += "\nvar data = " + data.to_json + ";" if data != nil
+    output += "\ndendrogram_main(i, \"" + parameters["param1"] + "\", data);"
+    output += "\n})();</script>"
+
+    output
+  end
+
+
+  # TODO: documentation - what this method does? change name
+  def get_data_for_dendrogram(experiment, id, param1)
+   # simulation_runs = experiment.simulation_runs.to_a
+    rinruby = Rails.configuration.r_interpreter
+
+    #getting data
+    result_file = Tempfile.new('dendrogram')
+    result_csv = create_result_csv
+
+    IO.write(result_file.path, result_csv)
+
+    #evaluate R commands
+    R.eval <<EOF
+    hdata <- hclust(dist(read.csv('#{result_file.path}')), 'complete')
+    merge <- hdata$merge
+    lst <- c(
+				height = hdata$height,
+				order = hdata$order,
+				labels = hdata$labels,
+				method = hdata$method
+        )
+EOF
+    #parameters
+    lst = R.pull "lst"
+    #structure of the tree (matrix)
+    merge = R.pull "merge"
+
+    #parsing matrix into hash: value -> left child, right child
+    hash = {}
+    for counter in 1..merge.row_size()
+      hash[counter.to_s] = [merge[(counter-1),0].to_i, merge[(counter-1),1].to_i]
+    end
+    hash
+    
+  end
+
+  def creating_dendrogram_structure(data)
+# search for - and - pairs and creating dict output value -> this pair
+
+
+
+
+  end
+  def moe_names
+    moe_name_set = []
+    limit = @experiment.size > 1000 ? @experiment.size / 2 : @experiment.size
+    @experiment.simulation_runs.where({ is_done: true }, { fields: %w(result), limit: limit }).each do |simulation_run|
+      moe_name_set += simulation_run.result.keys.to_a
+    end
+
+    moe_name_set.uniq
+  end
+
+  def parameters_names
+    parameters_names = []
+
+    @experiment.experiment_input.each do |entity_group|
+      entity_group['entities'].each do |entity|
+        entity['parameters'].each do |parameter|
+          parameters_names << @experiment.get_parameter_ids unless parameter.include?('in_doe') and parameter['in_doe'] == true
+        end
+      end
+    end
+
+    parameters_names
+  end
+
+  def create_result_csv(with_index=true, with_params=true, with_moes=true)
+    moes = moe_names
+    if with_params
+      all_parameters = parameters_names.uniq.flatten
+    end
+    CSV.generate do |csv|
+      header = []
+      header += ['simulation_index'] if with_index
+      header += all_parameters if with_params
+      header += moes if with_moes
+      csv << header
+
+      query_fields = {_id: 0}
+      query_fields[:index] = 1 if with_index
+      query_fields[:values] = 1 if with_params
+      query_fields[:result] = 1 if with_moes
+
+      @experiment.simulation_runs.where(
+          {is_done: true, is_error: {'$exists' => false}},
+          {fields: query_fields}
+      ).each do |simulation_run|
+        line = []
+        line += [simulation_run.index] if with_index
+        line += simulation_run.values.split(',') if with_params
+        # getting values of results in a specific order
+        line += moes.map { |moe_name| simulation_run.result[moe_name] || '' } if with_moes
+
+        csv << line
+      end
+    end
+  end
 end
