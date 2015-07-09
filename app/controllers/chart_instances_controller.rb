cbb0c214cef5bf65e43d7cc9c2d78224a3cb2239
khalska <katarzynahalska@wp.pl>
2015-07-09 08:01:40 +0200

    SCAL-825: The choice of meny moes in the dendrogram's window

--- app/controllers/chart_instances_controller.rb
+++ app/controllers/chart_instances_controller.rb
@@ -2,14 +2,22 @@ class ChartInstancesController < ApplicationController
   before_filter :load_experiment, only: :show
 
   def show
-    chart_id = params[:id].to_s #nazwa metody
+
+
+        chart_id = params[:id].to_s #nazwa metody
     experiment_id = params[:experiment_id].to_s
+
+    number_of_moes = params[:number_of_moes].to_i
+    param_tab = []
+    (0..number_of_moes-1).each do |i|
+       param_tab[i] = params[:"param#{i+1}"].to_s
+    end
 
     output = params[:output].to_s
     chart_counter = params[:chart_id].to_s
     type=  params[:type].to_s
-    param1 = params[:param1].to_s
-    param2 = params[:param2].to_s
+    param1 = param_tab[0].to_s
+    param2 = param_tab[1].to_s
 
     filter = {is_done: true, is_error: {'$exists'=> false}}
     fields = {fields: {result: 1}}
@@ -19,6 +27,7 @@ class ChartInstancesController < ApplicationController
     @parameters["chart_id"] = chart_counter
     @parameters["output"] = output
     @parameters["type"] = type
+    @parameters["param_tab"] = param_tab
     #moes and input parameters
     moes = @experiment.simulation_runs.where(filter, fields).first
     @parameters["moes"] = moes.blank? ? [] : moes.result
