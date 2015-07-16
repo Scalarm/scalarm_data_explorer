62738183b331fa69301760666452846e4bc32ff2
Andriu01 <romanski.andrzej@gmail.com>
2015-07-16 05:46:08 -0700

    Small fixes for cluster_infos

---
+++ app/controllers/cluster_infos_controller.rb
@@ -0,0 +1,22 @@
+class ClusterInfosController < ApplicationController
+  before_filter :load_experiment, only: :show
+
+  #Id -> experimentId; simulations => 2,4,6,1,7
+  def show
+    simulations = params[:simulations].split(',').map{|s| s.to_i}
+    if simulations.include? 0
+      raise "Error: simulation not exists"
+    end
+    clusterInfos = ClusterInfos.new(@experiment,simulations)
+    #Rails.logger.debug(Benchmark.measure{content = clusterInfos.evaluate})
+    content = clusterInfos.evaluate
+    respond_to do |format|
+      #change it for display
+      format.html {render :html => content}
+      format.json { render json: {status: 'ok', data: content.to_json } }
+
+    end
+
+  end
+
+end
