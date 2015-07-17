700d75bce74ef6d39c1e42210527dbebb96217e6
Andriu01 <romanski.andrzej@gmail.com>
2015-07-17 00:28:56 -0700

    removing not used methods

---
+++ app/models/cluster_infos.rb
@@ -0,0 +1,205 @@
+require 'rinruby'
+#require 'benchmark' only for time test
+class ClusterInfos
+
+  attr_accessor :simulations_index
+  attr_accessor :experiment
+
+  ##
+  # init class and R package
+  def initialize(experiment, simulations_index)
+    R.eval ("require ('e1071', quietly=TRUE)")
+
+    #getting data
+    @experiment = experiment
+    @simulations_index = simulations_index #[214,51,219,123,98]
+
+  end
+
+  ##
+  # function to get all data about simulations
+  def evaluate
+    header, result_data = create_data_result
+    datas =  statistics(header,result_data)
+    datas
+  end
+
+  ##
+  # main function which collect all data and return it as hash
+  def statistics(header,result_data)
+    datas = {}
+
+    for counter in 0..(header.count()-1)
+      #assign table once before executing
+      R.assign(header[counter],result_data.map{|row| row[counter]})
+      datas[header[counter]] = result_data.map{|row| row[counter]}
+
+    end
+    hash = {}
+
+    hash[:skewness] = evaluate_r_function(header, "skewness")
+    hash[:kurtosis] = evaluate_r_function(header, "kurtosis")
+    hash[:inter_quartile_ranges] = evaluate_r_function(header, "IQR")
+
+    #in Ruby gem these methods are faster
+    hash[:means] = calculate_function(header, datas,"mean")#evaluate_r_function(header, "mean")
+    hash[:medians] = calculate_function(header, datas,"median") #evaluate_r_quantile(header,3)
+
+    #differences with variance and standard_deviation results between R and Ruby method for now stay Ruby function
+    hash[:variances] = calculate_function(header, datas,"variance") #evaluate_r_function(header, "var")
+    hash[:standard_deviation] = calculate_function(header, datas,"standard_deviation")  #evaluate_r_function(header, "sd")
+
+    hash[:lower_quartiles] = evaluate_r_quantile(header,2)
+    hash[:upper_quartiles] = evaluate_r_quantile(header,4)
+
+    hash[:arguments_ranges] = arguments_ranges(header, datas)
+
+    hash
+  end
+
+
+  ##
+  # calculate by ruby descriptive statistics gem
+  # available functions:
+  # 1. Number
+  # 2 .Sum
+  # 3. Mean
+  # 4. Median
+  # 5. Mode
+  # 6. Variance
+  # 7. Standard Deviation
+  # 8. Percentile
+  # 9. Percentile Rank
+  # 10. Descriptive Statistics
+  # 11. Quartiles
+  def calculate_function(header, datas, function)
+    hash = {}
+    header.each do |arg|
+      param_data = datas[arg]
+      case function
+        when "mean"
+          hash[arg] = param_data.mean
+        when "variance"
+          hash[arg] = param_data.variance
+        when "median"
+          hash[arg] = param_data.median
+        when "standard_deviation"
+          hash[arg] = param_data.standard_deviation
+      end
+
+    end
+    hash
+
+  end
+
+  ##
+  # executing function in R by RinRuby gem
+  #passing array of arguments (in,out) and name of function
+  def evaluate_r_function(header, function)
+    hash = {}
+    header.each do |arg|
+
+      R.eval ("x <- #{function}(#{arg})")
+      value = R.pull "x"
+      hash[arg] = value
+    end
+    hash
+  end
+
+  ##
+  # calculating in R quantile
+  def evaluate_r_quantile(header, number)
+    hash = {}
+    header.each do |arg|
+
+      R.eval ("x <- quantile(#{arg})[[#{number}]]")
+      value = R.pull "x"
+      hash[arg] = value
+    end
+    hash
+  end
+
+  ##
+  # creating table of result data ranges
+  def arguments_ranges(header, result_data)
+    hash = {}
+    header.each do |arg|
+      param_data = result_data[arg]
+      hash[arg] = [param_data.min, param_data.max, param_data.max - param_data.min]
+    end
+    hash
+  end
+
+  ##
+  # get moes (results) names as an array
+  def moe_names
+    moe_name_set = []
+    limit = @experiment.size > 1000 ? @experiment.size / 2 : @experiment.size
+    @experiment.simulation_runs.where({ is_done: true }, { fields: %w(result), limit: limit }).each do |simulation_run|
+      moe_name_set += simulation_run.result.keys.to_a
+    end
+
+    moe_name_set.uniq
+  end
+
+  ##
+  # get input paramaters names as an array
+  def parameters_names
+    parameters_names = []
+
+    @experiment.experiment_input.each do |entity_group|
+      entity_group['entities'].each do |entity|
+        entity['parameters'].each do |parameter|
+          parameters_names << @experiment.get_parameter_ids unless parameter.include?('in_doe') and parameter['in_doe'] == true
+        end
+      end
+    end
+
+    parameters_names
+  end
+
+
+  ##
+  # no simulation index in header and with input and output paramameters
+  def create_data_result(with_index=false, with_params=true, with_moes=true)
+    moes = moe_names
+    if with_params
+      all_parameters = parameters_names.uniq.flatten
+    end
+    data_array=[]
+    header = []
+    header.push('simulation_index') if with_index
+    all_parameters.each do |value|
+      header.push(value)
+    end
+
+    moes.each do |moe|
+      header.push(moe)
+    end if with_moes
+
+    query_fields = {_id: 0}
+    query_fields[:index] = 1 if with_index
+    query_fields[:values] = 1 if with_params
+    query_fields[:result] = 1 if with_moes
+
+    @experiment.simulation_runs.where(
+        {is_done: true, is_error: {'$exists' => false}, 'index' => {'$in' => @simulations_index }},
+        {fields: query_fields}
+    ).each do |simulation_run|
+      line = []
+      line.push(simulation_run.index) if with_index
+      values = simulation_run.values.split(',')if with_params
+
+      values.each do |value|
+        line.push(value.to_f)
+      end
+      moes.map { |moe_name|
+        line.push(simulation_run.result[moe_name] || '') } if with_moes
+      data_array.push(line)
+
+    end
+
+    return header, data_array
+  end
+
+end
