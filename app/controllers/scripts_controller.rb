73116b5bfa7a1dbaf16069d28df17f8f13ed4c10
Andriu01 <romanski.andrzej@gmail.com>
2015-07-17 13:53:42 +0200

    13 to refactor

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -2,6 +2,16 @@ require 'erb'
 class ScriptsController < ApplicationController
   include ERB::Util
   def show
+    analysisMethodsConfig = AnalysisMethodsConfig.new
+    methods = analysisMethodsConfig.get_method_names
+    #validating chart_id (name of method)
+    validate(
+        id: Proc.new do |param_name, value|
+          unless methods.include? value
+            raise "Wrong chart name"
+          end
+        end
+    )
     #render experiment chart
     render file: Rails.root.join('app','visualisation_methods', ERB::Util.h(params[:id]), "chart.js"),
            layout: false,
