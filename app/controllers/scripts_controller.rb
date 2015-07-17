3e00200476547a2187b8db5bb0acd247a2eb526f
Andriu01 <romanski.andrzej@gmail.com>
2015-07-17 00:33:33 -0700

    3.2,3,5,12

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -1,8 +1,9 @@
+require 'erb'
 class ScriptsController < ApplicationController
- 
+  include ERB::Util
   def show
     #render experiment chart
-    render file: Rails.root.join('app','visualisation_methods', params[:id], "chart.js"),
+    render file: Rails.root.join('app','visualisation_methods', ERB::Util.h(params[:id]), "chart.js"),
            layout: false,
            content_type: 'text/javascript'
   end
