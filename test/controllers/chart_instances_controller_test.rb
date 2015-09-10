29c706001c75514064c37cc91789778f6d0c7d18
khalska <katarzynahalska@wp.pl>
2015-09-10 11:32:10 +0200

    SCAL-948: Remove mocks to setup and renamed tests.

--- test/controllers/chart_instances_controller_test.rb
+++ test/controllers/chart_instances_controller_test.rb
@@ -8,6 +8,28 @@ class ChartInstancesControllerTest < ActionController::TestCase
 
   def setup
     stub_authentication
+    experiment1_id = BSON::ObjectId.new
+
+    @simulation_run = mock do
+      stubs(:result).returns('1,2,3')
+    end
+
+    # inside the bloc mock do can not use instance variable
+    @simulation_runs = mock
+    @simulation_runs.stubs(:where).returns([@simulation_run])
+
+    @experiment = mock 'experiment' do
+      stubs(:id).returns(experiment1_id)
+      stubs(:start_at).returns(Time.now)
+      stubs(:completed?).returns(true)
+      stubs(:get_parameter_ids).returns(['a'])
+    end
+    @experiment.stubs(:simulation_runs).returns(@simulation_runs)
+
+    @visible_experiments = mock
+    @visible_experiments.stubs(:find_by_id).returns(@experiment)
+
+    Scalarm::Database::Model::Experiment.stubs(:visible_to).with(@user).returns(@visible_experiments)
   end
 
   def teardown
@@ -15,112 +37,51 @@ class ChartInstancesControllerTest < ActionController::TestCase
 
   end
 
-
-  test "should get show appropriate chart_id" do
-    experiment1_id = BSON::ObjectId.new
+  
+  test "should get show success" do
     ChartInstancesController.any_instance.expects(:require_plugin)
-
-
-    simulation_run = mock do
-      stubs(:result).returns('1,2,3')
-    end
-
-    simulation_runs = mock do
-      stubs(:where).returns([simulation_run])
-    end
-
-    experiment = mock 'experiment' do
-      stubs(:id).returns(experiment1_id)
-      stubs(:start_at).returns(Time.now)
-      stubs(:completed?).returns(true)
-      stubs(:simulation_runs).returns(simulation_runs)
-      stubs(:get_parameter_ids).returns(['a'])
-    end
-
-    visible_experiments = mock do
-      stubs(:find_by_id).returns(experiment)
-    end
-
+    experiment = @experiment
     ChartInstancesController.any_instance.expects(:generate_content_with_plugin).with('dendrogram', experiment, includes('chart_id' , 'output')).returns('<script>alert(\'1\')</script>')
-    Scalarm::Database::Model::Experiment.stubs(:visible_to).with(@user).returns(visible_experiments)
 
     get :show, id: 'dendrogram', experiment_id: experiment.id.to_s, chart_id: '0', output: 'product', using_em: 'false'
-
     assert_response :success
-    assert_equal 'dendrogram_chart_form', response.body[/\<section class='panel radius plot' id='(.*?)'>/,1]
-    assert_includes response.body, '/assets/application.css', nil
-
-
-
   end
 
-  test "should not apply application.js" do
-    experiment1_id = BSON::ObjectId.new
+  # there should be section with id like name of method in id
+  test "should load appropriate type of chart" do
     ChartInstancesController.any_instance.expects(:require_plugin)
-
-
-    simulation_run = mock do
-      stubs(:result).returns('1,2,3')
-    end
-
-    simulation_runs = mock do
-      stubs(:where).returns([simulation_run])
-    end
-
-    experiment = mock 'experiment' do
-      stubs(:id).returns(experiment1_id)
-      stubs(:start_at).returns(Time.now)
-      stubs(:completed?).returns(true)
-      stubs(:simulation_runs).returns(simulation_runs)
-      stubs(:get_parameter_ids).returns(['a'])
-    end
-
-    visible_experiments = mock do
-      stubs(:find_by_id).returns(experiment)
-    end
-
+    experiment = @experiment
     ChartInstancesController.any_instance.expects(:generate_content_with_plugin).with('dendrogram', experiment, includes('chart_id' , 'output')).returns('<script>alert(\'1\')</script>')
-    Scalarm::Database::Model::Experiment.stubs(:visible_to).with(@user).returns(visible_experiments)
+
+    get :show, id: 'dendrogram', experiment_id: experiment.id.to_s, chart_id: '0', output: 'product', using_em: 'false'
+    assert_equal 'dendrogram_chart_form', response.body[/\<section\s+class='panel\s+radius\s+plot'\s+id='(.*?)'>/,1]
+  end
+
+  test "should load css if using_em = false" do
+    ChartInstancesController.any_instance.expects(:require_plugin)
+    experiment = @experiment
+    ChartInstancesController.any_instance.expects(:generate_content_with_plugin).with('dendrogram', experiment, includes('chart_id' , 'output')).returns('<script>alert(\'1\')</script>')
+
+    get :show, id: 'dendrogram', experiment_id: experiment.id.to_s, chart_id: '0', output: 'product', using_em: 'false'
+    assert_includes response.body, '/assets/application.css', nil
+  end
+
+  test "should not load css if using_em = true" do
+    ChartInstancesController.any_instance.expects(:require_plugin)
+    experiment = @experiment
+    ChartInstancesController.any_instance.expects(:generate_content_with_plugin).with('dendrogram', experiment, includes('chart_id' , 'output')).returns('<script>alert(\'1\')</script>')
 
     get :show, id: 'dendrogram', experiment_id: experiment.id.to_s, chart_id: '0', output: 'product', using_em: 'true'
     refute_includes response.body, '/assets/application.css', nil
   end
 
-  test "should not apply application.js if using_em is not defined" do
-    experiment1_id = BSON::ObjectId.new
+  test "should not load css if using_em is not defined" do
     ChartInstancesController.any_instance.expects(:require_plugin)
-
-
-    simulation_run = mock do
-      stubs(:result).returns('1,2,3')
-    end
-
-    simulation_runs = mock do
-      stubs(:where).returns([simulation_run])
-    end
-
-    experiment = mock 'experiment' do
-      stubs(:id).returns(experiment1_id)
-      stubs(:start_at).returns(Time.now)
-      stubs(:completed?).returns(true)
-      stubs(:simulation_runs).returns(simulation_runs)
-      stubs(:get_parameter_ids).returns(['a'])
-    end
-
-    visible_experiments = mock do
-      stubs(:find_by_id).returns(experiment)
-    end
-
+    experiment = @experiment
     ChartInstancesController.any_instance.expects(:generate_content_with_plugin).with('lindev', experiment, includes('chart_id' , 'output')).returns('<script>alert(\'1\')</script>')
-    Scalarm::Database::Model::Experiment.stubs(:visible_to).with(@user).returns(visible_experiments)
 
     get :show, id: 'lindev', experiment_id: experiment.id.to_s, chart_id: '0', output: 'product'
     refute_includes response.body, '/assets/application.css', nil
   end
-
-
-
-
-
 
 end
